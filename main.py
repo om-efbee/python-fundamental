b00acc5aa2efdc1cb6b9d2b104f2682b442b0636
faroukbas <imajisetting@gmail.com>
2022-11-20 19:42:29 +0700

    test cek 2

---
+++ main.py
@@ -0,0 +1,20 @@
+# Percabangan
+
+jumlah_botol_susu = 173
+jumlah_telur = 0
+print(f'jumlah botol susu {jumlah_botol_susu} btl')
+print (f"jumlah telur {jumlah_telur} butir")
+
+
+if jumlah_botol_susu > 0:
+    print ('Budi mengecek harganya, dan ternyata uangnya cukup')
+    if jumlah_telur == 0:
+
+        print ('Budi membeli 1 botol susu')
+    else :
+        print("budi membeli 6 botol susu")
+else :
+    print ('Budi tidak jadi membeli 1 botol susu')
+
+print ('budi pulang ke rumah')
+print ('menyampaikan hasilnya kepada ibu')
