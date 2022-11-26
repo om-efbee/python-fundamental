11b495a7d057bb831ac6bed78e5de10b7aa94261
faroukbas <imajisetting@gmail.com>
2022-11-27 05:27:40 +0700

    belajar string

---
+++ app.py
@@ -0,0 +1,9 @@
+kursus = 'python untuk pemula banget'
+
+print (kursus.find ('a'))
+kursus2 = kursus.replace('untuk','bagi')
+print (kursus2.upper())
+print ('untuk' in kursus)
+print ('untuk' in kursus2)
+print (len (kursus))
+print (kursus.title())
